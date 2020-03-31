cc61073fa69a67ba435f1a80549304240ddbcb0d
Seungbin <noely1008@naver.com>
2020-03-31 22:22:48 +0900

    Tables, VB modified

--- APFS/Tables.cs
+++ APFS/Tables.cs
@@ -4,32 +4,70 @@ namespace APFS
 {
     public class Table
     {
-        public uint table_type;
+        public uint check_point;
         public uint btree_level;
+        public uint table_type;
+        public uint record_num;
         public uint len_record_def;
         public uint len_key_section;
         public uint gap_key_data;
-        public uint check_point;
-        public uint record_num;
+
 
         //static void Main()
         //{
-        //    using (FileStream fs = new FileStream(@"/Users/im-aron/Documents/한컴GMD/han.dmg", FileMode.Open))
+        //    using (FileStream fs = new FileStream(@"/Users/seungbin/Downloads/han.dmg", FileMode.Open))
         //    {
-        //        //CSB.MSB_Address = 20480;
+        //        CSB.MSB_Address = 20480;
         //        CSB.BlockSize = 4096;
-        //        UInt64 block_num = 323;
+        //        //UInt64 block_num = 323;
+        //        UInt64 block_num = 0;
         //        Table t = get_table_header(fs, block_num);
-        //        CSB.MSB_Address = Utility.get_string_address(fs, 0, "NXSB");
-        //        Console.WriteLine("CSB.MSB_Address = {0}", CSB.MSB_Address);
         //        //Console.WriteLine("table_type : {0}", t.table_type);
         //        //Console.WriteLine("len_record_def : {0}", t.len_record_def);
         //        //Console.WriteLine("len_key_section : {0}", t.len_key_section);
         //        //Console.WriteLine("gap_key_data : {0}", t.gap_key_data);
 
+        //        UInt64 VRB_addr = get_block_address(fs, block_num, "0xA0");
+        //        Console.WriteLine("VRB address : {0}", VRB_addr);
+
+        //        UInt64 VB_addr = get_block_address(fs, VRB_addr, "0x30");
+        //        Console.WriteLine("VB address : {0}", VB_addr);
+
+        //        t = get_table_header(fs, VB_addr);
+        //        Console.WriteLine("VB check_point : {0}", t.check_point);
+        //        Console.WriteLine("VB table_type : {0}", t.table_type);
+        //        Console.WriteLine("VB record_num : {0}", t.record_num);
+        //        Console.WriteLine("VB len_record_def : {0}", t.len_record_def);
+        //        Console.WriteLine("VB len_key_section : {0}", t.len_key_section);
+        //        Console.WriteLine("VB gap_key_data : {0}", t.gap_key_data);
+
+
+        //        VB.save_recodeVB(fs, VB_addr, t.table_type, t.record_num, t);
+
+
+
         //    }
 
         //}
+
+
+        public static UInt64 get_block_address(FileStream stream, UInt64 blocknum, string address)
+        {
+            UInt64 VRB_addr;
+            int n;
+            string hex;
+            byte[] buf = new byte[64];
+            UInt64 block_addr = Utility.get_address(blocknum);
+
+            stream.Seek((Int64)block_addr + Convert.ToInt64(address, 16), SeekOrigin.Begin);
+            n = stream.Read(buf, 0, 4);
+            hex = BitConverter.ToString(buf).Replace("-", String.Empty);
+            VRB_addr = (UInt64)Utility.little_hex_to_uint64(hex, n);
+            //VRB_addr = Utility.get_address(VRB_addr);
+
+            return VRB_addr;
+        }
+
 
         public static Table get_table_header(FileStream stream, UInt64 blocknum)
         {
@@ -41,39 +79,40 @@ namespace APFS
             UInt64 block_addr = Utility.get_address(blocknum);
 
             stream.Seek((Int64)block_addr + Convert.ToInt64("0x10", 16), SeekOrigin.Begin);
-            n = stream.Read(buf, 0, 8);
+            n = stream.Read(buf, 0, 2);
             hex = BitConverter.ToString(buf).Replace("-", String.Empty);
-            t.check_point = (uint)Utility.little_hex_to_uint64(hex, 2 * n);
+            t.check_point = (uint)Utility.little_hex_to_uint64(hex, n);
 
             stream.Seek((Int64)block_addr + Convert.ToInt64("0x18", 16), SeekOrigin.Begin);
-            n = stream.Read(buf, 0, 8);
+            n = stream.Read(buf, 0, 2);
             hex = BitConverter.ToString(buf).Replace("-", String.Empty);
-            t.btree_level = (uint)Utility.little_hex_to_uint64(hex, 2 * n);
+            t.btree_level = (uint)Utility.little_hex_to_uint64(hex, n);
 
             stream.Seek((Int64)block_addr + Convert.ToInt64("0x20", 16), SeekOrigin.Begin);
-            n = stream.Read(buf, 0, 8);
+            n = stream.Read(buf, 0, 2);
             hex = BitConverter.ToString(buf).Replace("-", String.Empty);
-            t.table_type = (uint)Utility.little_hex_to_uint64(hex, 2 * n);
+            t.table_type = (uint)Utility.little_hex_to_uint64(hex, n);
 
             stream.Seek((Int64)block_addr + Convert.ToInt64("0x24", 16), SeekOrigin.Begin);
-            n = stream.Read(buf, 0, 8);
+            n = stream.Read(buf, 0, 2);
             hex = BitConverter.ToString(buf).Replace("-", String.Empty);
-            t.record_num = (uint)Utility.little_hex_to_uint64(hex, 2 * n);
+            t.record_num = (uint)Utility.little_hex_to_uint64(hex, n);
 
+            // 바꾼거 
             stream.Seek((Int64)block_addr + Convert.ToInt64("0x2A", 16), SeekOrigin.Begin);
-            n = stream.Read(buf, 0, 8);
+            n = stream.Read(buf, 0, 2);
             hex = BitConverter.ToString(buf).Replace("-", String.Empty);
-            t.len_record_def = (uint)Utility.little_hex_to_uint64(hex, 2 * n);
+            t.len_record_def = (uint)Utility.little_hex_to_uint64(hex, n);
 
-            stream.Seek((Int64)block_addr + Convert.ToInt64("0x2C", 16), SeekOrigin.Begin);
-            n = stream.Read(buf, 0, 8);
+            //stream.Seek((Int64)block_addr + Convert.ToInt64("0x2C", 16), SeekOrigin.Begin);
+            n = stream.Read(buf, 0, 2);
             hex = BitConverter.ToString(buf).Replace("-", String.Empty);
-            t.len_key_section = (uint)Utility.little_hex_to_uint64(hex, 2 * n);
+            t.len_key_section = (uint)Utility.little_hex_to_uint64(hex, n);
 
-            stream.Seek((Int64)block_addr + Convert.ToInt64("0x2E", 16), SeekOrigin.Begin);
-            n = stream.Read(buf, 0, 8);
+            //stream.Seek((Int64)block_addr + Convert.ToInt64("0x2E", 16), SeekOrigin.Begin);
+            n = stream.Read(buf, 0, 2);
             hex = BitConverter.ToString(buf).Replace("-", String.Empty);
-            t.gap_key_data = (uint)Utility.little_hex_to_uint64(hex, 2 * n);
+            t.gap_key_data = (uint)Utility.little_hex_to_uint64(hex, n);
 
             return t;
         }
--- APFS/VB.cs
+++ APFS/VB.cs
@@ -1,19 +1,79 @@
 ﻿using System;
+using System.IO;
 
 namespace APFS
 {
-    public class VRB
-    {
-        public UInt64 VB; //0x30
-    }
 
-    public class VB
+    public struct VB
     {
-        public UInt16 TableType; //0x20
-        public UInt16 RecordNum; //0x24
-        public UInt16 tableIndexSize; //0x2a
-        public UInt16 tableKeyAreaSize; //0x2c
-        public UInt16 tableFreeSpaceSize; //0x2e
+        public static UInt64 VCSB_BlockNum; //0x20
 
+
+        public static void save_recodeVB(FileStream stream, UInt64 block_num, UInt64 Table_type, UInt64 record_num, Table header)
+        {
+            int n;
+            string hex;
+            byte[] buf = new byte[64];
+            UInt64 block_addr = Utility.get_address(block_num);
+
+            switch (Table_type)
+            {
+                case 7:
+
+                    TableType7[] records = new TableType7[record_num];
+                    for (ulong i = 0; i < record_num; i++)
+                    {
+                        records[i] = new TableType7();
+
+                        stream.Seek((Int64)block_addr + Convert.ToInt64("0x38", 16) + (Int64)i * 4, SeekOrigin.Begin);
+                        n = stream.Read(buf, 0, 2);
+                        hex = BitConverter.ToString(buf).Replace("-", String.Empty);
+                        records[i].KeyOffset = (UInt16)Utility.little_hex_to_uint64(hex, n);
+
+                        //stream.Seek((Int64)block_addr + Convert.ToInt64("0x3A", 16)+ (Int64)i*4, SeekOrigin.Begin);
+                        n = stream.Read(buf, 0, 2);
+                        hex = BitConverter.ToString(buf).Replace("-", String.Empty);
+                        records[i].DataOffset = (UInt16)Utility.little_hex_to_uint64(hex, n);
+
+                        //key section
+                        stream.Seek((Int64)block_addr + (Int64)header.len_record_def + (Int64)records[i].KeyOffset + Convert.ToInt64("0x38", 16), SeekOrigin.Begin);
+                        n = stream.Read(buf, 0, 8);
+                        hex = BitConverter.ToString(buf).Replace("-", String.Empty);
+                        records[i].NodeID = (UInt64)Utility.little_hex_to_uint64(hex, n);
+
+                        n = stream.Read(buf, 0, 8);
+                        hex = BitConverter.ToString(buf).Replace("-", String.Empty);
+                        records[i].StructureID = (UInt64)Utility.little_hex_to_uint64(hex, n);
+
+                        //data section
+                        stream.Seek((Int64)block_addr + 4096 - 40 - (Int64)records[i].DataOffset, SeekOrigin.Begin);
+                        stream.Read(buf, 0, 4);
+                        n = stream.Read(buf, 0, 4);
+                        hex = BitConverter.ToString(buf).Replace("-", String.Empty);
+                        records[i].BlockSize = (UInt16)Utility.little_hex_to_uint64(hex, n);
+
+                        n = stream.Read(buf, 0, 8);
+                        hex = BitConverter.ToString(buf).Replace("-", String.Empty);
+                        records[i].BlockNum = (UInt64)Utility.little_hex_to_uint64(hex, n);
+
+                        if (i == 0)
+                        {
+                            VCSB_BlockNum = records[i].BlockNum;
+                        }
+
+                        //Console.WriteLine("start : {0}", header.len_record_def);
+                        Console.WriteLine("recode key offset : {0}", records[i].KeyOffset);
+                        Console.WriteLine("recode data offset : {0}", records[i].DataOffset);
+                        Console.WriteLine("recode node id : {0}", records[i].NodeID);
+                        Console.WriteLine("recode structure id : {0}", records[i].StructureID);
+                        Console.WriteLine("recode block size: {0}", records[i].BlockSize);
+                        Console.WriteLine("recode block num: {0}", records[i].BlockNum);
+
+
+
+                    }
+                    break;
+            }
+        }
     }
 }
