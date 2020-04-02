c5f03d0328b005efa17b3f31652deb2728c37bc9
Seungbin <noely1008@naver.com>
2020-04-02 20:39:59 +0900

    finish record function

--- APFS/VB.cs
+++ APFS/VB.cs
@@ -14,9 +14,65 @@ namespace APFS
             string hex;
             byte[] buf = new byte[4096];
             UInt64 block_addr = Utility.get_address(block_num);
+            Int64 footer_length = 0; 
+            if (Table_type % 2 == 1)
+            {
+                //footer
+                Footer footer = new Footer();
+                stream.Seek((Int64)block_addr + 4096 - 16, SeekOrigin.Begin);
+                n = stream.Read(buf, 0, 8);
+                hex = BitConverter.ToString(buf).Replace("-", String.Empty);
+                footer.TotalLeafNode = (UInt16)Utility.little_hex_to_uint64(hex, n);
+                n = stream.Read(buf, 0, 8);
+                hex = BitConverter.ToString(buf).Replace("-", String.Empty);
+                footer.TotalIndexNode = (UInt16)Utility.little_hex_to_uint64(hex, n);
+                footer_length = 40;
+            }
 
             switch (Table_type)
             {
+                case 1:
+                    TableType1[] records1 = new TableType1[record_num];
+                    for (ulong i = 0; i < record_num; i++)
+                    {
+                        records1[i] = new TableType1();
+
+                        stream.Seek((Int64)block_addr + Convert.ToInt64("0x38", 16) + (Int64)i * 4, SeekOrigin.Begin);
+                        n = stream.Read(buf, 0, 2);
+                        hex = BitConverter.ToString(buf).Replace("-", String.Empty);
+                        records1[i].KeyOffset = (UInt16)Utility.little_hex_to_uint64(hex, n);
+
+                        n = stream.Read(buf, 0, 2);
+                        hex = BitConverter.ToString(buf).Replace("-", String.Empty);
+                        records1[i].KeyLength = (UInt16)Utility.little_hex_to_uint64(hex, n);
+
+                        n = stream.Read(buf, 0, 2);
+                        hex = BitConverter.ToString(buf).Replace("-", String.Empty);
+                        records1[i].DataOffset = (UInt16)Utility.little_hex_to_uint64(hex, n);
+
+                        n = stream.Read(buf, 0, 2);
+                        hex = BitConverter.ToString(buf).Replace("-", String.Empty);
+                        records1[i].DataLength = (UInt16)Utility.little_hex_to_uint64(hex, n);
+
+                        //key section
+                        stream.Seek((Int64)block_addr + (Int64)header.len_record_def + (Int64)records1[i].KeyOffset + Convert.ToInt64("0x38", 16), SeekOrigin.Begin);
+                        n = stream.Read(buf, 0, (int)(records1[i].KeyLength));
+                        hex = BitConverter.ToString(buf).Replace("-", String.Empty);
+                        hex = hex.Substring(0, records1[i].KeyLength);
+                        records1[i].IndexKey = hex;
+
+                        //data section
+                        stream.Seek((Int64)block_addr + 4096 - 40 - (Int64)records1[i].DataOffset, SeekOrigin.Begin);
+                        n = stream.Read(buf, 0, (int)(records1[i].DataLength));
+                        hex = BitConverter.ToString(buf).Replace("-", String.Empty);
+                        records1[i].NodeID = (UInt16)Utility.little_hex_to_uint64(hex, n);
+
+
+                        Console.WriteLine("{0}", i + 1);
+                        Console.WriteLine("record key offset : {0}", records1[i].KeyOffset);
+                    }
+                    break;
+
                 case 2:
                 case 3:
 
@@ -50,8 +106,8 @@ namespace APFS
                         records_23[i].KeySection = records_23[i].KeySection.Substring(0, records_23[i].KeyLength);
 
                         //data section
-                        stream.Seek((Int64)block_addr + 4096 - 40 - (Int64)records_23[i].DataOffset, SeekOrigin.Begin);
-                        n = stream.Read(buf, 0, (int)(records_23[i].DataLength / 8));
+                        stream.Seek((Int64)block_addr + 4096 - footer_length - (Int64)records_23[i].DataOffset, SeekOrigin.Begin);
+                        n = stream.Read(buf, 0, (int)(records_23[i].DataLength));
                         hex = BitConverter.ToString(buf).Replace("-", String.Empty);
                         records_23[i].DataSection = hex;
                         records_23[i].DataSection = records_23[i].DataSection.Substring(0, records_23[i].DataLength);
@@ -93,7 +149,7 @@ namespace APFS
                         records_45[i].StructureID = (UInt64)Utility.little_hex_to_uint64(hex, n);
 
                         //data section
-                        stream.Seek((Int64)block_addr + 4096 - 40 - (Int64)records_45[i].DataOffset, SeekOrigin.Begin);
+                        stream.Seek((Int64)block_addr + 4096 - footer_length - (Int64)records_45[i].DataOffset, SeekOrigin.Begin);
                         n = stream.Read(buf, 0, 8);
                         hex = BitConverter.ToString(buf).Replace("-", String.Empty);
                         records_45[i].BlockNum = (UInt64)Utility.little_hex_to_uint64(hex, n);
@@ -129,7 +185,7 @@ namespace APFS
                         records_67[i].StructureID = (UInt64)Utility.little_hex_to_uint64(hex, n);
 
                         //data section
-                        stream.Seek((Int64)block_addr + 4096 - 40 - (Int64)records_67[i].DataOffset, SeekOrigin.Begin);
+                        stream.Seek((Int64)block_addr + 4096 - footer_length - (Int64)records_67[i].DataOffset, SeekOrigin.Begin);
                         stream.Read(buf, 0, 4);
                         n = stream.Read(buf, 0, 4);
                         hex = BitConverter.ToString(buf).Replace("-", String.Empty);
@@ -154,18 +210,7 @@ namespace APFS
                     }
                     break;
             }
-            if (Table_type % 2 == 1)
-            {
-                //footer
-                Footer footer = new Footer();
-                stream.Seek((Int64)block_addr + 4096 - 16, SeekOrigin.Begin);
-                n = stream.Read(buf, 0, 8);
-                hex = BitConverter.ToString(buf).Replace("-", String.Empty);
-                footer.TotalLeafNode = (UInt16)Utility.little_hex_to_uint64(hex, n);
-                n = stream.Read(buf, 0, 8);
-                hex = BitConverter.ToString(buf).Replace("-", String.Empty);
-                footer.TotalIndexNode = (UInt16)Utility.little_hex_to_uint64(hex, n);
-            }
+            
         }
     }
 }
