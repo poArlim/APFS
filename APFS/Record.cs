2dbb81908aa2f57c72d03903ddfdfa61d5409b34
yejineee <21700431@handong.edu>
2020-04-06 20:30:39 +0900

    Overall structure of Recrod
    Signed-off-by: yejineee <21700431@handong.edu>

--- APFS/Record.cs
+++ APFS/Record.cs
@@ -1,7 +1,75 @@
 ﻿using System;
-
+using System.IO;
 namespace APFS
 {
+    public class RECORD
+    {
+        public FileFolderRecord[] ff;
+        public NameAttribute[] na;
+        public ExtentStatus[] es;
+        public ExtentRecord[] er;
+        public KeyRecord[] kr;
+
+        //public static void Main()
+        //{
+        //    using (FileStream fs = new FileStream(@"/Users/yang-yejin/Desktop/file_info/han.dmg", FileMode.Open))
+        //    {
+        //        CSB.MSB_Address = 20480;
+        //        CSB.BlockSize = 4096;
+  
+        //        init_btln(fs, 323); //148000
+
+        //    }
+        //}
+        public static RECORD init_btln(FileStream stream, UInt64 block_num)
+        {
+            RECORD btln = new RECORD();
+            Table header = Table.get_table_header(stream, block_num);
+            Console.WriteLine("block_addr : {0} record num :{1}",Utility.get_address(block_num), header.record_num);
+            TableType[] table_info = Table.save_record(stream, block_num, header);
+
+            Console.WriteLine("KeyOffset : {0}", table_info[0].KeyOffset);
+            Console.WriteLine("KeyLength : {0}", table_info[0].KeyLength);
+            Console.WriteLine("DataOffset : {0}", table_info[0].DataOffset);
+            Console.WriteLine("DataLength : {0}", table_info[0].DataLength);
+            Console.WriteLine("KeySection : {0}", table_info[0].KeySection);
+            Console.WriteLine("DataSection : {0}", table_info[0].DataSection);
+
+            for(int i = 0; i < header.record_num; i++)
+            {
+                char record_type = table_info[i].KeySection[14];
+                Console.WriteLine("[{0}] record_type : {1}", i+1, record_type);
+                switch (record_type)
+                {
+                    case '3':
+                        btln.ff = FileFolderRecord.get();
+                        break;
+
+                    case '4':
+                        btln.na = NameAttribute.get();
+                        break;
+
+                    case '6':
+                        btln.es = ExtentStatus.get();
+                        break;
+
+                    case '8':
+                        btln.er = ExtentRecord.get();
+                        break;
+
+                    case '9':
+                        btln.kr = KeyRecord.get();
+                        break;
+
+
+                }
+            }
+            return btln;
+        }
+
+
+    }
+ 
     public class FileFolderRecord // 0x30
     {
         //key section
@@ -25,6 +93,13 @@ namespace APFS
         public char[] FileName; //0x68
         public UInt64 ContentLenLog; //file바로 뒤
         public UInt64 ContentLenGross; //위에 이어서
+
+
+        public static FileFolderRecord[] get(string key, string data)
+        {
+           
+        }
+
     }
 
     public class NameAttribute // 0x40
@@ -32,6 +107,10 @@ namespace APFS
         //key section
 
         //data section
+        public static NameAttribute[] get(string key, string data)
+        {
+
+        }
     }
 
     public class ExtentStatus // 0x60
@@ -40,6 +119,11 @@ namespace APFS
 
         //data section
         public UInt32 ExtentExist; //0x00
+
+        public static ExtentStatus[] get(string key, string data)
+        {
+
+        }
     }
 
     public class ExtentRecord // 0x80
@@ -49,6 +133,11 @@ namespace APFS
         //data section
         public UInt64 ExtentLength; //0x00
         public UInt64 ExtentStartBlockNum; //0x08
+
+        public static ExtentRecord[] get(string key, string data)
+        {
+
+        }
     }
 
     public class KeyRecord // 90
@@ -61,5 +150,9 @@ namespace APFS
         public UInt64 CNID; //0x00
         public DateTime AddedDate; //0x08
 
+        public static KeyRecord[] get(string key, string data)
+        {
+
+        }
     }
 }
